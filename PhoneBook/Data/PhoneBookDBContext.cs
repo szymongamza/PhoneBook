41ee346707b32903205ecbab7652f54ea953f0e0
Szymon Gamża <gamza.szymon@gmail.com>
2022-09-04 01:37:21 +0200

    Add project files.

---
+++ PhoneBook/Data/PhoneBookDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Data
+{
+    public class PhoneBookDBContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\FlashCardsDB;Database=PhoneBookDb;Trusted_Connection=True;MultipleActiveResultSets=true;");
+
+        }
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using PhoneBook.Data;
+using PhoneBook.Data.Entities;
+using ConsoleTables;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var dbContext = new PhoneBookDBContext();
+            //var contact = new Contact { Name = "Arletta", Surname = "Gamża", PhoneNumber = "+48123456789", EmailAdress = "xyz@gmail.com"};
+            //dbContext.Contacts.Add(contact);
+            //_ = await dbContext.SaveChangesAsync();
+
+            Contact tempContact = dbContext.Contacts.Find(3);
+            dbContext.Remove(tempContact);
+            dbContext.SaveChanges();
+
+            var contacts = dbContext.Contacts;
+            Console.WriteLine(ConsoleTable.From(contacts));
+
+        }
+    }
+}
