11410ddc2c28cc5dfbfe51bed6ec683288e78772
Szymon Gamża <gamza.szymon@gmail.com>
2022-09-05 00:11:24 +0200

    Added controller

---
+++ PhoneBook/Controllers/ContactsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Data;
+using PhoneBook.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Controllers
+{
+    public class ContactsController
+    {
+        static PhoneBookDBContext dbContext = new PhoneBookDBContext(); 
+        public static async Task CreateContact(Contact contact)
+        {
+            dbContext.Add(contact);
+            await dbContext.SaveChangesAsync();
+        }
+        public static async Task<List<Contact>> GetContact()
+        {
+            var contacts = await dbContext.Contacts.ToListAsync();
+            return contacts;
+        }        
+        public static async Task<Contact?> GetContact(int id)
+        {
+            var contact = await dbContext.Contacts.FirstOrDefaultAsync(x => x.Id == id);
+            if( contact != null)
+            {
+                return contact;
+            }
+            return null;
+        }
+        public static async Task Update()
+        {
+            dbContext = new PhoneBookDBContext();
+        }
+        public static async Task DeleteContact(Contact contact)
+        {
+            dbContext.Remove(contact);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,6 +1,7 @@
 ﻿using PhoneBook.Data;
 using PhoneBook.Data.Entities;
 using ConsoleTables;
+using PhoneBook.Controllers;
 
 namespace PhoneBook
 {
@@ -8,17 +9,47 @@ namespace PhoneBook
     {
         static async Task Main(string[] args)
         {
-            var dbContext = new PhoneBookDBContext();
-            //var contact = new Contact { Name = "Arletta", Surname = "Gamża", PhoneNumber = "+48123456789", EmailAdress = "xyz@gmail.com"};
-            //dbContext.Contacts.Add(contact);
-            //_ = await dbContext.SaveChangesAsync();
-
-            Contact tempContact = dbContext.Contacts.Find(3);
-            dbContext.Remove(tempContact);
-            dbContext.SaveChanges();
-
-            var contacts = dbContext.Contacts;
-            Console.WriteLine(ConsoleTable.From(contacts));
+            bool exit = false;
+            while (exit == false)
+            {
+                Console.WriteLine(ConsoleTable.From(await ContactsController.GetContact()));
+                Console.WriteLine("+----------------------------+");
+                Console.WriteLine("| Press 0 to exit program    |");
+                Console.WriteLine("| Press 1 to add new contact |");
+                Console.WriteLine("| Press 2 to delete contact  |");
+                Console.WriteLine("| Press 3 to change contact  |");
+                Console.WriteLine("+----------------------------+");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        exit = true;
+                        break;
+                    case "1":
+                        Console.Clear();
+                        string tempName = Console.ReadLine();
+                        string tempSurname = Console.ReadLine();
+                        string tempPhoneNumber = Console.ReadLine();
+                        string tempEmail = Console.ReadLine();  
+                        Contact tempContact = new Contact {
+                            Name = tempName,
+                            Surname = tempSurname,
+                            PhoneNumber = tempPhoneNumber,
+                            EmailAdress = tempEmail
+                        };
+                        await ContactsController.CreateContact(tempContact);
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        //change
+                        break;
+                    default:
+                        Console.WriteLine("Choose option from menu!");
+                        break;
+                }
+            }
 
         }
     }
